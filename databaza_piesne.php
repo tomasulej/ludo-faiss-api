efca8de23677d99faaf6ca63c4be2bd5ed8f2b70
Tomas Ulej1 <ludo@ludoslovensky.sk>
2016-06-24 16:02:48 +0200

    uprava pre lokalne pracovanie

--- databaza_piesne.php
+++ databaza_piesne.php
@@ -1,10 +1,24 @@
 <?php
+
+if ($_SERVER['APP_ENV']=='tomas') {
+      $pmeno="root";
+      $heslo="LudoLudoVedMaNeser";
+      $databaza="piesne";
+      $host="localhost";
+
+
+
+} else {
       $pmeno="piesne";
       $heslo="LudoLudoVedMaNeser";
       $databaza="piesne";
       $host="localhost";
 
+
+}
       $konstDie = "Chyba pri spojení s databázou. Zlý, zlý Ulej! Ty radšej nič neprogramuj, ale Ulej!";
+
+
                                        //Konstanty
       mysql_connect($host,$pmeno,$heslo);
       mysql_select_db($databaza) or die($konstDie);
