2fbfc8a9a71fcf991d6f28c5b30676c748384fb0
Tomas Ulej1 <ludo@ludoslovensky.sk>
2016-07-25 16:28:16 +0200

    sss

--- vyzva/index.php
+++ vyzva/index.php
@@ -172,7 +172,7 @@ ini_set('display_errors', '1');
 
     
     //include "../src/mail/MailGun.php";
-require 'vendor/autoload.php';
+require '../vendor/autoload.php';
 
 $mgClient = new Mailgun('key-4c498788dbbce6979a5225f131cea166');
 $domain = "ludoslovensky.sk";
