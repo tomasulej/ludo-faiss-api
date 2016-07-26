bc9f1b7065c9c0da0eb62dcebeb20c6fbe9d4844
Roman Klco <klco.roman@gmail.com>
2016-07-26 13:30:31 +0200

    loga

--- vyzva/index.php
+++ vyzva/index.php
@@ -348,24 +348,24 @@ ini_set('display_errors', '1');
             <p>(c) Ľudo Slovenský 2013-2016. Web: <a href="http://www.facebook.com/tomas.ulej.pise">Tomáš Ulej</a> a <a href="http://romanklco.com">Roman Klčo</a></p>
             
 
-            <div class="row"><div class="col-md-9">
+            <div class="row"><div class="col-md-7">
             <div class="l-footer__partners">
 
                 <h2>Partneri a sponzori:</h2>
                 <a href="http://www.martinus.sk"><img src="/public/img/martinus-inverse.png"></a>
-                <a href="http://www.websupport.sk"><img src="/public/img/websupport-inverse.png" ></a>
+                <a href="http://www.websupport.sk"><img src="/public/img/websupport-inverse.png"></a>
                 <a href="http://www.mestskakniznica.sk"><img src="/public/img/kniznica-inverse.png"></a>
                 <a href="http://www.jankohrasko.sk"><img src="/public/img/janko-hrasko-inverse.png"></a>
 
             </div>
             </div>
 
-            <div class="col-md-3">
+            <div class="col-md-5">
             <div class="l-footer__partners">
 
                 <h2>Odborný garant:</h2>
 
-                <a href="http://www.ketnoffukf.sk"><img src="/public/img/ukf.png" height="150px"><br>Katedra etnológie a folkloristiky UKF v Nitre</a>
+                <a href="http://www.ketnoffukf.sk"><img src="/public/img/ukf.png"><br>Katedra etnológie a folkloristiky UKF v Nitre</a>
 
 
             </div>
