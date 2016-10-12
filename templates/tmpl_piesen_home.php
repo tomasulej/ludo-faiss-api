c7e058adf8664f9b923c10d5d87827988b1bb6cd
Tomas Ulej1 <ludo@ludoslovensky.sk>
2016-10-12 20:45:29 +0200

    titulka nastrel

--- templates/tmpl_piesen_home.php
+++ templates/tmpl_piesen_home.php
@@ -44,23 +44,23 @@ require $_SERVER["DOCUMENT_ROOT"]."/templates/tmpl_piesne_header_home.php";
 
 
 
-<div class="row">
-<div class="col-md-12">
+<div class="container"><div class="row l-page">
+<div class="col-md-8">
 <h2>Najnovšie pridané piesne</h2>
 
 <?php foreach ($piesne as $key=>$piesen) { ?>
         <div class="col-md-12">
         <div class="l-song-item l-well"> 
-        <h3><a href="piesen.php?<?php echo $piesen['id_piesen'];?>"><?php echo ($piesen["nazov_kratky"]==""?"(ešte nezdigitalizované)":$piesen["nazov_dlhy"]."…");?></a></h3>
+        <h3><a href="piesen.php?<?php echo $piesen['id_piesen'];?>"><?php echo ($piesen["nazov_dlhy"]==""?"(ešte nezdigitalizované)":$piesen["nazov_dlhy"]."…");?></a></h3>
         <?php if ($piesen['file_mp3']<>"") { 
             $p_button="playpause_p_".$piesen['id_piesen'];
             $p_audio="aud_".$piesen['id_piesen'];
             ?> 
 
-        <a class="l-btn l-btn--primary l-btn--small" id="<?php echo $p_button;?>" 
+<!--        <a class="l-btn l-btn--primary l-btn--small" id="<?php echo $p_button;?>" 
             onclick="playpause('<?php echo "#".$p_audio;?>','<?php echo "#".$p_button;?>');" ><i class="fa fa-play"></i></a>
         <audio id="<?php echo $p_audio; ?>" controls="controls" src="data/<?php echo $piesen['id_piesen']; ?>/<?php echo $piesen['file_mp3']; ?>" style="display:none">Your browser does not support the audio element.</audio>
-
+-->
         <?php }?>
 
         <a href="piesen.php?<?php echo $piesen['id_piesen'];?>"><img src='data/<?php echo $piesen['id_piesen']; ?>/<?php echo $piesen["file_png"];?>'></a>
@@ -69,16 +69,86 @@ require $_SERVER["DOCUMENT_ROOT"]."/templates/tmpl_piesne_header_home.php";
         </p>-->
         </div><BR>
 
+
+
         </div><div></div>
     <?php } ?>
 
+<div align="center"><button type="submit" class="l-btn l-btn--large l-btn--primary">Všetky piesne (<?php echo $pocet_piesni;?>)</button></div>
+
+</div>
+
+
+<div class="col-md-4">
+<H2>Najpopulárnejšie piesne</H2>
+<ol>
+<?php foreach ($piesne_top as $key=>$piesen) { ?>
+    <li><a href="piesen.php?<?php echo $piesen['id_piesen'];?>"><?php echo $piesen["nazov_dlhy"]?></a></li>
+
+<?php } ?> 
+</ol>
+
+<div align="center"><button type="submit" class="l-btn l-btn--large l-btn--primary">Viac populárnych piesni (<?php echo $pocet_piesni;?>)</button></div>
 
 
 </div>
 
 
+</div>
+
+<div class="row">
+<div class="col-md-12">
+<H2>Mapa pokrytia regiónov (odkiaľ všade už máme piesne)? <a href="/piesne/mapa.php">Pozrieť celú mapu</a></H2>
+<div id="mapa" class="map" style="height:450px"></div>
+<script>
+                        L.mapbox.accessToken = 'pk.eyJ1IjoiamVsdXNhbW90IiwiYSI6ImNpZnN0NGM2MjAxd2N1NGx6OWk2Y3BjOGsifQ.aFGe3wpK5fbZbrpefXxDNA';
+                        var map = L.map('mapa').setView([48.812,19.473], 8);
+                        var baseLayer=L.tileLayer(
+                            'https://api.mapbox.com/styles/v1/jelusamot/citx7z0my00aj2irqjs36mmeh/tiles/256/{z}/{x}/{y}?access_token=' + L.mapbox.accessToken, {
+                            tileSize: 512,
+                            zoomOffset: -1,
+                         }).addTo(map);
+
+
+
+                        <?php foreach ($p_mapa_point as $key=>$point) { ?>
+                                    var p_<?php echo $point["c"]; ?> = [
+                                        <?php echo $point["area"]; ?>
+
+                                    ];
+
+
+                                    var myLayer_p_<?php echo $point["c"] ?> = L.mapbox.featureLayer(p_<?php echo $point["c"] ?>, {
+                                        pointToLayer: function(feature, latlon) {
+                                           
+  
+                                            return L.marker(latlon, {
+                                                fillColor:  '#66CC00',
+                                                'paint': {
+                                                    'fill-color': 'blue',
+                                                    'fill-opacity': 0.8,
+                                                },
+                                                fillOpacity: 1.0,
+                                                stroke: true
+                                            });
+                                        }
+                                    }).addTo(map);
+
+
+
+
+                        <?php } ?>
+</script>
+
 
 </div>
+</div>
+<BR>
+
+
+
+
+</div></div>
 
 
 
