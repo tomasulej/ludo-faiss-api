aaeebc7de332e1fa1cf958882650dbe4db01d243
Tomas Ulej1 <ludo@ludoslovensky.sk>
2016-11-29 23:22:34 +0100

    no message

--- prislovia2/hladat.php
+++ prislovia2/hladat.php
@@ -1,8 +1,8 @@
 <?php
 //includes
 
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+//error_reporting(E_ALL);
+//ini_set('display_errors', '1');
 
 include $_SERVER["DOCUMENT_ROOT"]."/databaza_prislovia.php";
 include "kategoria.inc.php";
