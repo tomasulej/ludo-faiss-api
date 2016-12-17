0dd245baf6b5b029ec4f6a58212eb8a4ff21645c
Tomas Ulej1 <ludo@ludoslovensky.sk>
2016-12-17 08:32:19 +0100

    seo

--- prislovia/hladat.php
+++ prislovia/hladat.php
@@ -12,11 +12,19 @@ include "kategoria.inc.php";
 
 
 
-
 $q=$_GET['q'];
 
 $tmpl_prislovie= "<p id=\"prislovie_%s\"><a class='prislovie' data-toggle=\"popover\"  
 data-content=\"<div class='quote-popover'>%s</div>\" href=\"prislovie.php?id=%s\">%s</a></p>";
+
+
+//META
+$meta_type="article";
+$meta_title="Ľudo Slovenský: príslovia a porekadlá na tému ".$q;
+$meta_image="http://www.ludoslovensky.sk/public/img/ludo-prislovia.png";
+$meta_url="http://".$_SERVER['SERVER_NAME']."/prislovia/hladat.php?".$_GET['q'];
+$meta_desc="Ľudo slovenský radí a poradí aj Tebe. Príď sa pozrieť na 12-tisíc jeho porekadiel a prísloví";
+
 
 
 
