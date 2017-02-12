6722fba552ba38150e2165e0aac60bc5d2f2e5e8
Tomas Ulej1 <ludo@ludoslovensky.sk>
2017-02-12 20:43:48 +0100

    no message

--- piesne/index.php
+++ piesne/index.php
@@ -33,7 +33,7 @@ $pocet_piesni=mysql_num_rows($query2);
 //piesne najpopularnejsie
 
 $datum=date('Y-m-d', strtotime('-20 days'))." 00:00:00";
-$query3=mysql_query("SELECT log.id_piesen, piesne.nazov_dlhy, sum(log.pocet) as videnia FROM log LEFT JOIN piesne ON piesne.id_piesen=log.id_piesen WHERE datumcas > $datum AND id_nadriadeny=0 GROUP BY id_piesen ORDER BY videnia DESC LIMIT 20");
+$query3=mysql_query("SELECT log.id_piesen, piesne.nazov_dlhy, sum(log.pocet) as videnia FROM log LEFT JOIN piesne ON piesne.id_piesen=log.id_piesen WHERE datumcas > '$datum' AND id_nadriadeny=0 GROUP BY id_piesen ORDER BY videnia DESC LIMIT 20");
 $piesne_top=array();
 while ( $riadok = mysql_fetch_array($query3)) {
   array_push($piesne_top, $riadok);
