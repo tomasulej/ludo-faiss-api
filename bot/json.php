60fafeda0c487629a3a56edbd653ed043005d7fb
Tomas Ulej1 <ludo@ludoslovensky.sk>
2017-03-13 20:56:01 +0100

    no message

--- bot/json.php
+++ bot/json.php
@@ -1,6 +1,46 @@
-{
-"messages": [
-{"text": "Welcome to our store!"},
-{"text": "How can I help you?"}
-]
+<?php
+
+//includes
+include $_SERVER["DOCUMENT_ROOT"]."/databaza_prislovia.php";
+
+$tmpl_odpoved='{
+  "messages": [
+    {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "button",
+          "text": "%s",
+          "buttons": [
+            {
+              "type": "show_block",
+              "block_name": "prislovie",
+              "title": "Ďalšie príslovie, prosím!"
+            },
+            {
+              "type": "web_url",
+              "url": "https://ludoslovensky.sk/prislovia/prislovie.php?id=%s",
+              "title": "Pozrieť toto príslovie na webe"
+            }
+          ]
+        }
+      }
+    }
+  ]
+}';
+
+
+$co=$_GET['co'];
+
+if ($co=='prislovie') {
+    $q=mysql_query("SELECT * FROM pr_txt WHERE (hlasy>2)  ORDER BY RAND() LIMIT 1");
+    $prislovie=mysql_fetch_object($q);
+    printf($tmpl_odpoved, $prislovie->txt,$prislovie->id);
+
 }
+
+
+
+?>
+
+
