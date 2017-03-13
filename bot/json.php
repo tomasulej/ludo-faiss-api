65d896a7741f8b139e4043b119b63d74c7c87dbe
Tomas Ulej1 <ludo@ludoslovensky.sk>
2017-03-13 21:07:48 +0100

    no message

--- bot/json.php
+++ bot/json.php
@@ -5,23 +5,23 @@ include $_SERVER["DOCUMENT_ROOT"]."/databaza_prislovia.php";
 
 $tmpl_odpoved='{
   "messages": [
-    {"text": "%s"},
+    {"text": "-----------------"},
     {
       "attachment": {
         "type": "template",
         "payload": {
           "template_type": "button",
-          "text": "Hello!",
+          "text": "%s",
           "buttons": [
             {
               "type": "show_block",
-              "block_name": "some block name",
-              "title": "Show the block!"
+              "block_name": "prislovie",
+              "title": "Zobraz ďalšiu radu"
             },
             {
               "type": "web_url",
-              "url": "https://petersapparel.parseapp.com/buy_item?item_id=%s",
-              "title": "Buy Item"
+              "url": "https://ludoslovensky.sk/prislovia/prislovie.php?id=%s",
+              "title": "Zdieľať s priateľmi"
             }
           ]
         }
