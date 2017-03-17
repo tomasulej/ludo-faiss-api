0bb1ac0b2066fe7c7aed56d9308043a83c380fb1
Tomas Ulej1 <ludo@ludoslovensky.sk>
2017-03-17 20:51:36 +0100

    no message

--- piesne/analyze/json_piesen_contour.php
+++ piesne/analyze/json_piesen_contour.php
@@ -3,6 +3,11 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
+include $_SERVER["DOCUMENT_ROOT"]."/databaza_piesne.php";
+$id_piesen=$_GET['id_piesen'];
+$q=mysql_query("SELECT * FROM piesne WHERE id_piesen=$id_piesen;");
+$piesen=mysql_fetch_object($q);
+
 $command = escapeshellcmd('/usr/bin/python /var/www/html/piesne/analyze/contour.py /var/www/html/piesne/data/'.$piesen->id_piesen.'/'.$piesen->file_xml);
 echo $command;
 
