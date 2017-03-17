dbe1595d00f2e7d5275e5fc9c4fc21ee131105d7
Tomas Ulej1 <ludo@ludoslovensky.sk>
2017-03-17 22:27:21 +0100

    no message

--- templates/tmpl_piesen_main.php
+++ templates/tmpl_piesen_main.php
@@ -341,18 +341,21 @@
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                Melódia
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;x</span>
-                </button>
+
+
 
             </div>
             <div class="modal-body" id="contour_div">
 
 
             </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary" data-dismiss="modal">Zatvoriť</button>
 
+      </div>
         </div>
+
+
     </div>
 </div>
 
