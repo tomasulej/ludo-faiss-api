b5791dd6007426236d3656f9a3b42af57f41fdbb
Tomas Ulej1 <ludo@ludoslovensky.sk>
2017-03-17 22:21:59 +0100

    no message

--- piesne/analyze/contour.php
+++ piesne/analyze/contour.php
@@ -2,6 +2,11 @@
 	$id_piesen=$_GET['id_piesen'];
 ?>
 
+
+<script type="text/javascript">
+	json_file='json_piesen_contour.php?id_piesen=<?php echo $id_piesen; ?>';
+</script>
+
     <script type='text/javascript' src="/public/js/d3.min.js" charset="utf-8"></script>
     <link rel='stylesheet' type='text/css' href='/piesne/analyze/style.css'>
 	<script type='text/javascript' src='/piesne/analyze/script.js'></script>
@@ -13,8 +18,5 @@
 		<div id = 'buttons'>
 		</div>
 
-<script type="text/javascript">
-	json_file='json_piesen_contour.php?id_piesen=<?php echo $id_piesen; ?>';
-</script>
 
 
