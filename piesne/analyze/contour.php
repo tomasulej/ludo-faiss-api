e0b0efb004991c2f2821861d38e968dae03bdf52
Tomas Ulej1 <ludo@ludoslovensky.sk>
2017-03-19 15:48:53 +0100

    no message

--- piesne/analyze/contour.php
+++ piesne/analyze/contour.php
@@ -13,7 +13,7 @@
     <link rel='stylesheet' type='text/css' href='/piesne/analyze/style.css'>
 	<script type='text/javascript' src='/piesne/analyze/script.js'></script>
 		<div class = 'container'>
-		<p class="popis"><small>Prvá redakcia zbierky Slovenské spevy urobila v melódii tejto piesne zmeny oproti originálu, ktorý jej zaslal pôvodný zberateľ . Ladislav Galko, autor druhého vydania Slovenských spevov, preskúmal a porovnal originál piesne s publikovanou verziu a zaznamenal zmeny.<a class="contour-url" href="">Zobraziť, ako pieseň zverejnila prvá redakcia</a></small></p>
+		<p class="popis"><small>Prvá redakcia zbierky Slovenské spevy urobila v melódii tejto piesne zmeny oproti originálu, ktorý jej zaslal pôvodný zberateľ. Ladislav Galko, autor druhého vydania Slovenských spevov, preskúmal a porovnal originál piesne s publikovanou verziu a zaznamenal zmeny. Ak chcete zobraziť celú pieseň tak, ako ju zverejnila prvá redakcia, <a class="contour-url" href="">kliknite sem</a></small></p>
 
 	    	<svg class='chart'>
 	    	</svg>
