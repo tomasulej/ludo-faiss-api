e75f991f87ba1c973baea1a3eca27ee55451d706
Tomas Ulej1 <ludo@ludoslovensky.sk>
2017-03-26 14:59:19 +0200

    no message

---
+++ blog/o-menach-v-ludovych-piesnach.php
@@ -0,0 +1,22 @@
+<?php
+//includes
+include $_SERVER["DOCUMENT_ROOT"]."/databaza_piesne.php";
+
+
+//META
+$meta_type="article";
+$meta_title="Ako (ne)nazvať svoje dieťa (O kom sa spieva v ľudových piesňach)";
+$meta_image="http://www.ludoslovensky.sk/public/img/ludo-music.png";
+$meta_url="http://".$_SERVER['SERVER_NAME']."/piesne/hladat.php";
+$meta_desc="Kataríny (od jedného promile vyššie) zvyknú v krčmách spievať \"Neďaleko od Trenčína\",  Jánovia (v rovnakom stave) zase \"Nepi, Jano, nepi vodu\". O ktorých menách sa najčastejšie spieva v slovenských ľudových piesňach?";
+
+
+
+
+$telo="<script id=\"infogram_0_ludo___mena-68560\" title=\"Ľudo - mená\" src=\"//e.infogr.am/js/dist/embed.js?x9o\" type=\"text/javascript\"></script><div style=\"padding:8px 0;font-family:Arial!important;font-size:13px!important;line-height:15px!important;text-align:center;border-top:1px solid #dadada;margin:0 30px\"><a href=\"https://infogr.am/ludo___mena-68560\" style=\"color:#989898!important;text-decoration:none!important;\" target=\"_blank\" rel=\"nofollow\">Ľudo - mená</a><br><a href=\"https://infogr.am/create/column-chart?utm_source=embed_bottom&utm_medium=seo&utm_campaign=column_chart\" style=\"color:#989898!important;text-decoration:none!important;\" target=\"_blank\" rel=\"nofollow\">Create column charts</a></div>";
+
+$bg_delete=true;
+// nacitanie informacii
+require ($_SERVER["DOCUMENT_ROOT"]."/templates/tmpl_blog_article.php");
+
+?>
---
+++ templates/tmpl_blog_article.php
@@ -0,0 +1,46 @@
+<?php
+    $theme="l-theme-blue";
+require $_SERVER["DOCUMENT_ROOT"]."/templates/tmpl_header.php";
+//require $_SERVER["DOCUMENT_ROOT"]."/templates/tmpl_ludo_header.php";
+
+?>
+<div class="l-page">
+
+    <div class="container">
+
+        <div class="row">
+
+
+
+<!--<script src="//cdn.jsdelivr.net/medium-editor/latest/js/medium-editor.min.js"></script>
+<link rel="stylesheet" href="//cdn.jsdelivr.net/medium-editor/latest/css/medium-editor.min.css" type="text/css" media="screen" charset="utf-8">
+-->
+
+            <div class="offset-md-1 col-md-10" id="telo">
+
+ <?php echo $telo;?>
+                
+
+            </div>
+
+<!-- <script>var editor = new MediumEditor('#telo');</script> -->
+
+
+   
+        </div>
+
+    </div>
+
+
+</div>
+
+
+
+
+
+<?php
+
+require $_SERVER["DOCUMENT_ROOT"]."/templates/tmpl_footer.php";
+
+
+?>
