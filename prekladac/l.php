466baa2e71383a592b39d5f8e772d62aca9c32ae
Tomas Ulej1 <ludo@ludoslovensky.sk>
2017-04-08 17:30:18 +0200

    no message

--- prekladac/l.php
+++ prekladac/l.php
@@ -1,7 +1,7 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
+//error_reporting(E_ALL);
+//ini_set('display_errors', '1');
 
 $url=$_GET['url'];
 $command = escapeshellcmd("/usr/bin/python /var/www/html/prekladac/ludevit/examples/www/fetch.py $url -");
