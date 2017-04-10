c9acce37026d9269b56bcc7acd8b84f9c310946f
Tomas Ulej1 <ludo@ludoslovensky.sk>
2017-04-10 21:50:36 +0200

    no message

--- templates/tmpl_prekladac_index.php
+++ templates/tmpl_prekladac_index.php
@@ -11,7 +11,7 @@ if ($url<>"") { ?>
 
 
 
-<div class="embed-responsive embed-responsive-1by1" style="overflow: auto; -webkit-overflow-scrolling: touch;">
+<div class="embed-responsive embed-responsive-1by1 row" style="overflow: auto; -webkit-overflow-scrolling: touch;">
     <iframe class="embed-responsive-item col-lg-12 col-md-12 col-sm-12" style="overflow: auto; -webkit-overflow-scrolling: touch;" src="http://www.ludoslovensky.sk/prekladac/l.php?url=<?php echo $url;?>" allowfullscreen width="100%"></iframe>
 </div>
 
