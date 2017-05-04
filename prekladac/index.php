48390508924834eb3e1a72315d979e6c355b6d2f
Tomas Ulej1 <ludo@ludoslovensky.sk>
2017-05-04 20:39:57 +0200

    no message

--- prekladac/index.php
+++ prekladac/index.php
@@ -4,7 +4,7 @@
 $meta_type="article";
 $meta_title="Štúrovski prekladač: surfuj ako Ľudevít Štúr | Ľudo Slovenský ";
 $meta_image="http://www.ludoslovensky.sk/public/img/surfuj.png";
-$meta_url="http://".$_SERVER['SERVER_NAME']."/piesne/hladat.php";
+$meta_url="http://".$_SERVER['SERVER_NAME']."/prekladac/";
 $meta_desc="Koňjec ipsilonu! Takto bi bez ňeho Slovenskí internet vizerau. Nože viskúšaj!";
 
 require ($_SERVER["DOCUMENT_ROOT"]."/templates/tmpl_prekladac_index.php");
