3e94335c56efa1402124e4d6c0f0ec05dc5ca23f
Tomas Ulej1 <ludo@ludoslovensky.sk>
2017-05-04 20:38:20 +0200

    no message

--- prekladac/index.php
+++ prekladac/index.php
@@ -2,11 +2,10 @@
 
 //META
 $meta_type="article";
-$meta_title="Ľudo Slovenský: surfuj ako Ľudevít Štúr. ";
-$meta_image="http://www.ludoslovensky.sk/public/img/ludo-music.png";
+$meta_title="Štúrovski prekladač: surfuj ako Ľudevít Štúr | Ľudo Slovenský ";
+$meta_image="http://www.ludoslovensky.sk/public/img/surfuj.png";
 $meta_url="http://".$_SERVER['SERVER_NAME']."/piesne/hladat.php";
-$meta_desc="Tisíce rokov Ľudo Slovenský spieva piesne. Sú našim pokladom. Aby sme ho zachovali, postupne zbierame, digitalizujeme, triedime a sprístupňujeme desaťtisíce piesní, ktoré sa počas tisícok rokov spievali na našom území.";
-
+$meta_desc="Koňjec ipsilonu! Takto bi bez ňeho Slovenskí internet vizerau. Nože viskúšaj!";
 
 require ($_SERVER["DOCUMENT_ROOT"]."/templates/tmpl_prekladac_index.php");
 
