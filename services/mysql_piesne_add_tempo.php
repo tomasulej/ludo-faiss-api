cd57ddcf141f85184d59fbe24af5f42993a121fd
Tomas Ulej1 <ludo@ludoslovensky.sk>
2017-11-26 11:37:58 +0100

    no message

--- services/mysql_piesne_add_tempo.php
+++ services/mysql_piesne_add_tempo.php
@@ -7,7 +7,7 @@
   $q=mysql_query("SELECT * FROM piesne WHERE tempo_bpm IS NULL AND stav=1");
 
   while ($piesen=mysql_fetch_object($q)) {
-    $id=$piesen->id_piesen;
+    $id=$piesen->id_piesen; 
     $xml=simplexml_load_file($_SERVER["DOCUMENT_ROOT"]."/piesne/data/".$piesen->id_piesen."/".$piesen->file_xml);  
     $tempo_bpm=$xml->part[0]->measure[0]->direction->sound['tempo'];  
     //echo "ID: $piesen->id_piesen ==>".$xml->part[0]->measure[0]->direction->sound['tempo'].'<BR>';
