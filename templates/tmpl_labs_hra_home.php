7b35ae827488b682323a617c3852feb77c81dd16
Tomas Ulej1 <ludo@ludoslovensky.sk>
2017-12-15 20:22:50 +0100

    no message

--- templates/tmpl_labs_hra_home.php
+++ templates/tmpl_labs_hra_home.php
@@ -69,12 +69,13 @@ require $_SERVER["DOCUMENT_ROOT"]."/templates/tmpl_header.php";
 </div>
 </div>
 
-<div id="fb_chvastacka" style="display:none">
+<div class="row">
+<div id="fb_chvastacka" class="col-md-11 offset-md-1" style="display:none">
 
     <h5>Pochváľ sa, koľko si nahral(a)</h5>
     <div class="fb-comments" data-href="http://www.ludoslovensky.sk/labs/kviz/" data-numposts="5"></div>
 </div>
-
+</div>
 
 
     <script type="text/javascript">
