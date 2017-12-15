f6d7aae5d94b672edfb7a8cbdabc7e3c706a0a7e
Tomas Ulej1 <ludo@ludoslovensky.sk>
2017-12-15 21:13:29 +0100

    no message

--- labs/index.php
+++ labs/index.php
@@ -8,7 +8,7 @@ include $_SERVER["DOCUMENT_ROOT"]."/databaza_piesne.php";
 $meta_type="article";
 $meta_title="Ľudo Slovenský: prostonárodné ľudové laboratórium.";
 $meta_image="http://www.ludoslovensky.sk/public/img/ludo-labs.png";
-$meta_url="http://".$_SERVER['SERVER_NAME']."/labs/";
+$meta_url="http://www.".$_SERVER['SERVER_NAME']."/labs/kviz";
 $meta_desc="Laboratórium prostonárodných ľudových webových stránok. Nože skús.";
 
 
