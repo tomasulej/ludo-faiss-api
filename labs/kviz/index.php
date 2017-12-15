54dced2df94ef397fd10ed43b0a21132ec6a556c
Tomas Ulej1 <ludo@ludoslovensky.sk>
2017-12-15 21:14:58 +0100

    no message

--- labs/kviz/index.php
+++ labs/kviz/index.php
@@ -4,7 +4,7 @@
 $meta_type="article";
 $meta_title="Ľudov slovný kvíz: otestuj svoju slovnú zásobu";
 $meta_image="http://ludoslovensky.sk/public/img/labs_slova-statistiky.png";
-$meta_url="http://".$_SERVER['SERVER_NAME']."/labs/kviz";
+$meta_url="http://www.".$_SERVER['SERVER_NAME']."/labs/kviz";
 $meta_desc="Zisti, ako dobrý si v slovenčine. Vyzývam Ťa na absolvovanie Ľudovho slovného kvízu. Koľko významov nájdeš za minútu?";
 
 
