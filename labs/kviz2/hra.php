44d18e338bacc5e9a336ac99e33cb1d22234a272
Tomas Ulej1 <ludo@ludoslovensky.sk>
2018-11-06 17:45:17 +0100

    no message

--- labs/kviz2/hra.php
+++ labs/kviz2/hra.php
@@ -1,7 +1,7 @@
 <?php
 
-//error_reporting(E_ALL);
-//ini_set('display_errors', '1');
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
 
 
@@ -11,7 +11,7 @@ include $_SERVER["DOCUMENT_ROOT"]."/databaza_slova.php";
 
 // spravna odpoved;
 $counter=0;
-$q1=mysql_query("SELECT * FROM kviz_words WHERE count_synonyms>2 ORDER BY rand() LIMIT 10 ");
+$q1=mysql_query("SELECT * FROM kviz_words WHERE count_synonyms>2 ORDER BY rand() LIMIT 20 ");
 while ($objSolution=mysql_fetch_object($q1)) {
     $arrSolution=explode(";", $objSolution->synonyms);
     //shuffle($arrSolution);
