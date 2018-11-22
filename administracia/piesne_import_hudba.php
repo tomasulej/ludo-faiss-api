45cde1aff4ffc27c58985774df4c4cd6e1c665ec
Tomas Ulej1 <ludo@ludoslovensky.sk>
2018-11-22 21:03:58 +0100

    no message

--- administracia/piesne_import_hudba.php
+++ administracia/piesne_import_hudba.php
@@ -1,9 +1,18 @@
 <?php
 
-	//error_reporting(E_ALL);
-	//ini_set('display_errors', '1');
+	error_reporting(E_ALL);
+	ini_set('display_errors', '1');
 
 //nacitaj subory do spravneho priecinka
+
+function background($Command, $Priority = 0){
+    if($Priority)
+        $PID = shell_exec("nohup nice -n $Priority $Command > /dev/null & echo $!");
+    else
+        $PID = shell_exec("nohup $Command > /dev/null & echo $!");
+    return($PID);
+}
+
 
 //vezmi xml
 $fileList = glob('/var/www/html/piesne/import/*.mscz');
@@ -11,16 +20,16 @@ $fileList = glob('/var/www/html/piesne/import/*.mscz');
 foreach($fileList as $f){
     //Simply print them out onto the screen.
 
-    echo shell_exec("whoami")."PHP hovori: ".get_current_user()."<BR>";
+    //echo shell_exec("whoami")."PHP hovori: ".get_current_user()."<BR>";
 
     echo "Našiel som súbor <B>".$f."</B><br><BR>"; 
     $f_= pathinfo($f, PATHINFO_FILENAME);
     //Generujem k nemu xml,
 
-    $x=exec("Xvfb :0 -screen 0 1280x768x24& 2>&1",$vystup);
-    echo "Xvfb :0 -screen 0 1280x768x24& vrátilo <u>".var_dump($vystup).var_dump($x)."</u><BR>";
-    $x=exec("export DISPLAY=:0 2>&1",$vystup);
-    echo print_r($vystup);
+    echo background("Xvfb :0 -screen 0 1280x768x24& 2>&1");
+    //echo "Xvfb :0 -screen 0 1280x768x24& vrátilo <u>".var_dump($vystup).var_dump($x)."</u><BR>";
+    //$x=exec("export DISPLAY=:0 2>&1",$vystup);
+    //echo print_r($vystup);
 
     //echo "export DISPLAY=:0 vrátilo".var_dump($vystup).var_dump($x)."<BR>";
     $command = exec(escapeshellcmd("/usr/bin/mscore $f -o /var/www/html/piesne/import/$f_.xml 2>&1"), $vystup);
