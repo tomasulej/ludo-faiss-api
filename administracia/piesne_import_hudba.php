b3f5860cfb8b791b4e567c8815c9e7b5de1d02bd
Tomas Ulej1 <ludo@ludoslovensky.sk>
2018-11-22 20:36:00 +0100

    no message

--- administracia/piesne_import_hudba.php
+++ administracia/piesne_import_hudba.php
@@ -21,8 +21,7 @@ foreach($fileList as $f){
     //echo "Xvfb :0 -screen 0 1280x768x24& vrátilo <u>".var_dump($vystup).var_dump($x)."</u><BR>";
     $x=exec("export DISPLAY=:0 2>&1",$vystup);
     echo "export DISPLAY=:0 vrátilo".var_dump($vystup).var_dump($x)."<BR>";
-    $command = "/usr/bin/mscore $f -o /var/www/html/piesne/import/$f_.xml 2>&1";
-    $vystup = exec($command, $co);
+    $command = shell_exec("sudo /usr/bin/mscore $f -o /var/www/html/piesne/import/$f_.xml 2>&1");
     echo "Spustil som príkaz <i>".$command."</i>, server mi vrátil hlášku:".$vystup."xx".var_dump($co)."'.<BR>";
  
 }
