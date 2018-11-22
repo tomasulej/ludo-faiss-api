b3f9bc626c6469b146bfcdbe6551913d02645737
Tomas Ulej1 <ludo@ludoslovensky.sk>
2018-11-22 18:30:44 +0100

    no message

--- administracia/piesne_import_hudba.php
+++ administracia/piesne_import_hudba.php
@@ -17,8 +17,10 @@ foreach($fileList as $f){
     $f_= pathinfo($f, PATHINFO_FILENAME);
     //Generujem k nemu xml,
 
-    echo shell_exec("sudo Xvfb :0 -screen 0 1280x768x24&");
-    echo shell_exec("export DISPLAY=:0");
+    $x=exec("sudo Xvfb :0 -screen 0 1280x768x24&",$vystup);
+    echo "sudo Xvfb :0 -screen 0 1280x768x24& vrátilo".$vystup."<BR>";
+    $x=exec("export DISPLAY=:0",$vystup);
+    echo "export DISPLAY=:0 vrátilo".$vystup."<BR>";
     $command = "/usr/bin/mscore $f -o /var/www/html/piesne/import/$f_.xml 2>&1";
     $vystup = exec($command, $co);
     echo "Spustil som príkaz <i>".$command."</i>, server mi vrátil hlášku:".var_dump($vystup)."xx".var_dump($co)."'.<BR>";
