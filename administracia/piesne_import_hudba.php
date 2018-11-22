8c9fe2f646b97a4755fccebe589d1b8f45e59440
Tomas Ulej1 <ludo@ludoslovensky.sk>
2018-11-22 18:42:00 +0100

    no message

--- administracia/piesne_import_hudba.php
+++ administracia/piesne_import_hudba.php
@@ -18,12 +18,12 @@ foreach($fileList as $f){
     //Generujem k nemu xml,
 
     $x=exec("sudo Xvfb :0 -screen 0 1280x768x24& 2>&1",$vystup);
-    echo "sudo Xvfb :0 -screen 0 1280x768x24& vrátilo <u>".$vystup."</u><BR>";
+    echo "sudo Xvfb :0 -screen 0 1280x768x24& vrátilo <u>".var_dump($vystup).var_dump($x)."</u><BR>";
     $x=exec("export DISPLAY=:0 2>&1",$vystup);
-    echo "export DISPLAY=:0 vrátilo".$vystup."<BR>";
-    $command = "/usr/bin/mscore $f -o /var/www/html/piesne/import/$f_.xml 2>&1";
+    echo "export DISPLAY=:0 vrátilo".var_dump($vystup).var_dump($x)."<BR>";
+    $command = "sudp /usr/bin/mscore $f -o /var/www/html/piesne/import/$f_.xml 2>&1";
     $vystup = exec($command, $co);
-    echo "Spustil som príkaz <i>".$command."</i>, server mi vrátil hlášku:".$vystup."xx".$co."'.<BR>";
+    echo "Spustil som príkaz <i>".$command."</i>, server mi vrátil hlášku:".$vystup."xx".var_dump($co)."'.<BR>";
  
 }
 
