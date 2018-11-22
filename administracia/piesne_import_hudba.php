92ce88c4c19c88d3d51dfe36da188315fe7dbc46
Tomas Ulej1 <ludo@ludoslovensky.sk>
2018-11-22 18:35:06 +0100

    no message

--- administracia/piesne_import_hudba.php
+++ administracia/piesne_import_hudba.php
@@ -11,14 +11,14 @@ $fileList = glob('/var/www/html/piesne/import/*.mscz');
 foreach($fileList as $f){
     //Simply print them out onto the screen.
 
-    echo shell_exec("whoami");
+    //echo shell_exec("whoami");
 
-    echo "Našiel som súbor <B>".$f."</B><br>"; 
+    echo "Našiel som súbor <B>".$f."</B><br><BR>"; 
     $f_= pathinfo($f, PATHINFO_FILENAME);
     //Generujem k nemu xml,
 
     $x=exec("sudo Xvfb :0 -screen 0 1280x768x24&",$vystup);
-    echo "sudo Xvfb :0 -screen 0 1280x768x24& vrátilo".var_dump($vystup)."<BR>";
+    echo "sudo Xvfb :0 -screen 0 1280x768x24& vrátilo <u>".var_dump($vystup)."</u><BR>";
     $x=exec("export DISPLAY=:0",$vystup);
     echo "export DISPLAY=:0 vrátilo".var_dump($vystup)."<BR>";
     $command = "/usr/bin/mscore $f -o /var/www/html/piesne/import/$f_.xml";
