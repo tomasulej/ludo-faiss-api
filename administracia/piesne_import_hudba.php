cb447b031977fd29e1fa02a2cacc175b497efd5f
Tomas Ulej1 <ludo@ludoslovensky.sk>
2018-11-22 18:14:39 +0100

    no message

--- administracia/piesne_import_hudba.php
+++ administracia/piesne_import_hudba.php
@@ -10,10 +10,13 @@ $fileList = glob('/var/www/html/piesne/import/*.mscz');
 
 foreach($fileList as $f){
     //Simply print them out onto the screen.
+
+    echo shell_exec("whoami");
+
     echo "Našiel som súbor <B>".$f."</B><br>"; 
     $f_= pathinfo($f, PATHINFO_FILENAME);
     //Generujem k nemu xml,
-    $command = "sudo Xvfb :0 -screen 0 1280x768x24&; export DISPLAY=:0; sudo /usr/bin/mscore $f -o /var/www/html/piesne/import/$f_.xml 2>&1";
+    $command = "sudo Xvfb :0 -screen 0 1280x768x24&; export DISPLAY=:0; sudo /usr/bin/mscore $f -o /var/www/html/piesne/import/$f_.xml";
     $vystup = shell_exec($command);
     echo "Spustil som príkaz <i>".$command."</i>, server mi vrátil hlášku: '".$vystup."'.<BR>";
  
