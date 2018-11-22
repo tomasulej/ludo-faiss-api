c181c54e9b4e75076b1008924271b7b357a6da62
Tomas Ulej1 <ludo@ludoslovensky.sk>
2018-11-22 17:39:08 +0100

    no message

--- administracia/piesne_import_hudba.php
+++ administracia/piesne_import_hudba.php
@@ -13,9 +13,9 @@ foreach($fileList as $f){
     echo "Našiel som súbor <B>".$f."</B><br>"; 
     $f_= pathinfo($f, PATHINFO_FILENAME);
     //Generujem k nemu xml,
-    $command = escapeshellcmd("/usr/bin/mscore $f -o $f_.xml");
+    $command = escapeshellcmd("/usr/bin/mscore /var/www/html/piesne/import/$f -o /var/www/html/piesne/import/$f_.xml");
     $vystup = shell_exec($command);
-    echo "Pokúsil som sa k nemu vygenerovať XML, server mi vrátil hlášku: '".$vystup."'.<BR>";
+    echo "Spustil som príkaz <i>".$command."</i>, server mi vrátil hlášku: '".$vystup."'.<BR>";
  
 }
 
