5b894705cc0abeb02d4180207034ed5f097a7616
Tomas Ulej1 <ludo@ludoslovensky.sk>
2018-11-22 20:56:29 +0100

    no message

--- administracia/piesne_import_hudba.php
+++ administracia/piesne_import_hudba.php
@@ -17,13 +17,13 @@ foreach($fileList as $f){
     $f_= pathinfo($f, PATHINFO_FILENAME);
     //Generujem k nemu xml,
 
-    $x=exec("Xvfb :0 -screen 0 1280x768x24&",$vystup);
+    $x=exec("Xvfb :0 -screen 0 1280x768x24& 2>&1",$vystup);
     echo "Xvfb :0 -screen 0 1280x768x24& vrátilo <u>".var_dump($vystup).var_dump($x)."</u><BR>";
-    $x=exec("export DISPLAY=:0",$vystup);
+    $x=exec("export DISPLAY=:0 2>&1",$vystup);
     echo print_r($vystup);
 
     //echo "export DISPLAY=:0 vrátilo".var_dump($vystup).var_dump($x)."<BR>";
-    $command = exec(escapeshellcmd("/usr/bin/mscore $f -o /var/www/html/piesne/import/$f_.xml"), $vystup);
+    $command = exec(escapeshellcmd("/usr/bin/mscore $f -o /var/www/html/piesne/import/$f_.xml 2>&1"), $vystup);
     echo "Spustil som príkaz <i>/usr/bin/mscore $f -o /var/www/html/piesne/import/$f_.xml</i>, server mi vrátil hlášku:".$command.print_r($vystup)."'.<BR>";
  
 }
