28e040922264be73636e36535f92f59a0f854567
Tomas Ulej1 <ludo@ludoslovensky.sk>
2018-11-23 14:11:23 +0100

    no message

--- administracia/piesne_import_hudba.php
+++ administracia/piesne_import_hudba.php
@@ -32,8 +32,8 @@ foreach($fileList as $f){
 
     
     $x = shell_exec("sudo sh /var/www/html/administracia/musescore.sh $f /var/www/html/piesne/import/$f_");
-    $abc = shell_exec(escapeshellcmd("/usr/bin/python /var/www/html/public/py/xml2abc.py /var/www/html/piesne/import/$f_ 2>&1"));
-    echo "<pre>".print_r($abc)."</pre>";
+    $abc = shell_exec(escapeshellcmd("sudo /usr/bin/python /var/www/html/public/py/xml2abc.py /var/www/html/piesne/import/$f_ 2>&1"));
+    echo "<pre>".$abc."</pre>";
 
     
 }
