3423e09d96960a0fd22faf5bd40259f9408ddf0f
Tomas Ulej1 <ludo@ludoslovensky.sk>
2018-11-23 14:16:37 +0100

    no message

--- administracia/musescore.sh
+++ administracia/musescore.sh
@@ -4,5 +4,4 @@ export DISPLAY=:0
 /usr/bin/mscore $1 -o $2.xml
 /usr/bin/mscore $1 -o $2.mp3
 /usr/bin/mscore $1 -o $2.png
-
-
+sudo /usr/bin/python /var/www/html/public/py/xml2abc.py /var/www/html/piesne/import/$2.xml > $2.abc
