c6d8305a5e4fb4a545a789f1c501bc1664f16db6
Tomas Ulej1 <ludo@ludoslovensky.sk>
2018-11-29 19:51:10 +0100

    no message

--- administracia/piesne_import.php
+++ administracia/piesne_import.php
@@ -14,14 +14,14 @@ foreach($fileList as $f){
     echo "Našiel som súbor <B>".$f."</B><br>"; 
     $f_= pathinfo($f, PATHINFO_FILENAME);
     $f__=pathinfo($f,PATHINFO_BASENAME);
-    $fArr = explode("_", $f_);
+    $fArr = explode("_", $f__);
     $fId = $fArr[0]; 
     echo "V názve súboru som identifikoval identifikátor $fId.<BR>";
     $query="SELECT * FROM piesne WHERE identifikator='$fId'";
     $q=mysql_query($query);
     $piesen=mysql_fetch_object($q);
     echo "Identifikoval som pieseň, jej ide je $piesen->id_piesen a možno viem aj názov - '$piesen->nazov_dlhy'.<BR>";
-    $nf=$_SERVER["DOCUMENT_ROOT"].'/piesne/data/'.$piesen->id_piesen.'/'.$f__;
+    $nf=$_SERVER["DOCUMENT_ROOT"].'/piesne/data/'.$piesen->id_piesen.'/'.$fArr[1];
     mkdir($_SERVER["DOCUMENT_ROOT"].'/piesne/data/'.$piesen->id_piesen.'/', 0700);
     array_map('unlink', array_filter((array) glob($_SERVER["DOCUMENT_ROOT"].'/piesne/data/'.$piesen->id_piesen.'/*')));
     echo "Pokúšam sa ho presunúť do adresára ".$nf.".<BR>";
