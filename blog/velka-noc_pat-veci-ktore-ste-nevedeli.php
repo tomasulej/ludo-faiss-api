fce9fddae11de9acfc0f99ed9fc754ae0b093ff7
Tomas Ulej1 <ludo@ludoslovensky.sk>
2019-04-17 14:20:28 +0200

    no message

---
+++ blog/velka-noc_pat-veci-ktore-ste-nevedeli.php
@@ -0,0 +1,101 @@
+<?php
+//includes
+include $_SERVER["DOCUMENT_ROOT"]."/databaza_piesne.php";
+$blog_tab='class="active"';
+
+
+//META
+$meta_type="article";
+$meta_title="Päť vecí, ktoré ste nevedeli o Veľkej noci?";
+$meta_image="http://www.ludoslovensky.sk/public/img/ludo_infografika.png";
+$meta_url="http://".$_SERVER['SERVER_NAME']."/blog/o-menach-v-ludovych-piesnach.php";
+$meta_desc="Kataríny (od jedného promile vyššie) zvyknú v krčmách spievať Neďaleko od Trenčína,  Jánovia (v rovnakom stave) zase Nepi, Jano, nepi vodu. O ktorých menách sa najčastejšie spieva v slovenských ľudových piesňach?";
+
+
+
+
+$telo="
+
+<H1>Päť vecí, ktoré ste nevedeli o Veľkej noci</H1>
+<p><strong>Ako trávili Veľkú noc naši predkovia? Tu je päť vecí o najväčších kresťanských sviatkoch roka.</strong></p>
+
+<img src='/public/img/ludo_blog_dievcata_bubliny.png' class='img-fluid'>
+
+<h2>1. Pre bahniatka máme minimálne dvanásť rôznych výrazov</h2>
+
+<li><p><strong>Bahniatka</strong>: mali množstvo názvov - napríklad aj púzalky, maňky, maňušky, barky, birky, búziky, baburence, kozičky, kočičky, cícky, mládniky. „Na okolí Zvolena okiadzali vŕbovými púčikmi chorého, ktorého bolelo hrdlo. Na Orave dávali púčiky do prvej brázdy, na východnom Slovensku ich uložili k prvému zasadenému zemiaku. Na Horehroní vyháňali takýmto prútom dobytok, keď išiel po prvý raz na jar von zo stajne,“ píše Emília Horváthová v knihe Rok vo zvykoch nášho ľudu. </p></li>
+
+___STEADY_PAYWALL___
+
+<H2>2. Množstvo zvykov Veľkej noci nie je kresťanských</H2>
+<p>Aj keď sa súčasné sviatky <strong>spájajú najmä s kresťanskou tradíciou</strong>, mnohé z jej prvkov sú oveľa staršie. Vyskytovali sa na našom území už skôr a boli také obľúbené, že ich potom do svojich rituálov zaradila aj katolícka cirkev. </p>
+
+<p>Dobrým príkladom je oheň. Indoeurópania videli na jar v roznietení, založení nového čistého ohňa symbol znovuzrodenia. <i>„Kresťanská cirkev v úsilií nahradiť (tieto rituály) vlastným obradom zaviedla v ôsmom storočí veľkonočné svätenie ohňa,“</i> píše Emília Horváthová v knihe Rok vo zvykoch nášho ľudu.</p>
+
+<p>Obdobne to bolo s vodou, pričom samotné oblievačky ako ich poznáme dnes sú už len torzom toho, ako vodu vnímali naši predkovia.  </p>
+
+
+<h2>3. Kúpavalo sa aj v iné dni ako pondelok</h2>
+
+<p>Kým pre nás z kúpania ostala už len zábava, naši predkovia verili, že v období veľkej noci má voda moc liečiť. V čase Veľkej noci sa zďaleka nekúpavalo len na Veľkonočný pondelok. Na Horehroní sa napríklad kúpavalo už na Bielu Sobotu a to muži a ženy navzájom, do rieky sa vodil dokonca aj dobytok. Ľudia si v rieke vyplachovali aj ústa, aby nemali zubné kazy, keď bol niekto chorý, priviedli ho do rieky a veľkononočná voda ho mala vyliečiť. Viera, že voda je na Veľkú noc posvätná sa prejavovalo aj tak, že si ju ľudia odkladali na neskoršie použitie. „Kto sa chystal stavať nový dom, odložil si z nej do fľaštičky a túto v príhodnej chvíli zamuroval do základov,“ píše Horváthová. </p>
+
+<p>Čarovnú atmosféru veľkonočného obdobia sa usilovali využiť aj vydajachtivé dievčatá. Emília Horváthová vo svojej knihe spomína tradíciu z okolia Nitry: dievča, ktoré sa chcelo vydať išlo v noci zo Zeleného štvrtku na Veľký piatok ku kaluži a ponárajúc sa do nej holým zadkom odriekalo:</p>
+
+<blockquote class='blockquote'>„Kaluža, kaluža,<BR>
+dajže mi ty muža.<BR>
+Ak mi muža nedáš,<BR>
+na rok ma zas tu máš!“</blockquote>
+
+<h2>4. Prečo sa Zelený štvrtok volá Zelený?</h2>
+Prečo bol Zelený štvrtok zelený? Podľa niektorých ľudových výkladov kvôli jedlu, ktoré sa v ten deň jedlo - v Tekove zaniesli do kostola posvätiť chren a koreňovú zeleninu, z ktorej potom na obed navarili polievku. V južnejších oblastiach, kde už vegetácia pokročila varili šťavel, špenát alebo prívarok z listov mladej žihľavy, ktorá vraj ozdravovala krv. <i>„Žena, ktorá by v tento deň piekla chlieb, zapríčinila by veľké sucho a tým aj neúrodu,“</i> spomína Horváthová.</p>
+
+<p>Jedálny lístok na Veľký piatok bol zostavený podobne skromne ako na Zelený štvrtok. Zvyšok Veľkej noci sa už jedlo hojne a často obed vyzeral podobne ako na Vianoce. </p>
+
+
+<H2>5. Čo jedli ľudia na Veľkú noc?</H2>
+
+<p>V jedlách Veľkej noci prevládala bravčovina - lebo mala magicko-obradnú funkciu - konkrétne mala ľuďom dodať silu, šunka však bola oveľa menej častá ako dnes. Okrem toho nesmeli chýbať klobásy, jaternice alebo huspenina. Jahňa sa do jedálnička našich predkov dostalo zo židovskej kultúry. Zajac sa ako veľkonočné jedlo dostal na jedálniček až neskôr a vyskytoval sa skôr v mestách - vzišiel zo starogermánskej tradície - ako symbol plodnosti, bol kultovým zvieraťom starogermánskej bohyne úrody Ostary (od jej mena je odvodený aj nemecký názov Veľkej noci - Ostern). </p>
+
+<p>Špecialitou našich končín je tradičné veľkonočné jedlo z bieleho kysnutého cesta - malo u nás rôzne podoby a aj názvy po celom Slovensku - na východnom a čiastočne strednom Slovensku sa volalo „paska“, na Orave a v Liptove „baba“, v Tekove „calta“ v Novohrade „mrváň“, na Ponitrí „osúch“, inde „beluš“ alebo jednoducho „koláč“. </p>
+
+<p>—————</p>
+
+
+
+<div class='l-well'>
+<h2>Kto kúpal a kto šibal?</h2>
+
+<p><strong>Pred I. svetovou vojnou  </strong></p>
+<img class='img-fluid' src='/public/img/ludo_blog_chlapci.png'>
+
+<p> </p>
+
+<p>OKOLIE BRATISLAVY A NA ZÁHORÍ - rovnako známe šibanie aj polievanie. Mladí chlapci šibali na Veľkonočnú nedeľu - najmä gazdiné, „aby neoprašiveli“. Dospelí radšej sedeli doma, pretože ktorákoľvek žena ich stretla vonku mohla ich vyšľahať korbáčom. V pondelok sa pokračovalo v šibačke. </p>
+
+<p>PONITRIE - Na Ponitrí starší mládenci začali kúpať v nedeľu populudní a končili nadránom. Pokračovali v pondelok poobede. V každom dome kam prišli bolo pre nich pripravené bohaté pohostenie, takže niet divu, že potrebovali dva dni. Používali ručne zhodovené „striekačky“.</p>
+
+<p>OKOLIE POVAŽSKEJ BYSTRICE Na okolí Považskej Bystrice bola rozšírená polievačka aj šibačka, no mládenci nechodili po domoch ale striehli na dievčatá na ulici. Trvalo to týždeň. </p>
+
+
+<p>V obci ZUBEREC ženy kúpali v nedeľu mužov, v pondelok to bolo opačne a v utorok ženy a dievčatá mužov šibali. Rozdiel bol v tom, že mládenci chodili do domov, kde ich pohostili a obdarovali, zatiaľ čo ženy a dievčatá striehli na ulici. </p></li>
+<p>V oravských obciach situovaných bližšie ku Kysuciam chodili mládenci kúpať na Veľkonočný pondelok včasráno - prakticky ešte v noci, menší chlapci dopoludnia. Na Orave smerom na východ šibanie ustupuje pred polievaním. </p></li>
+<p>Kúpanie i šibačky boli rovnaké vo všetkých horehronských obciach s výnimkou Polomky. Aj tu sa začala v pondelok včasráno, takmer hneď po polnoci „kuparská&quot;. ktorá sa zvyčajne pretiahla na celé popoludnie. Pred večerom v dome, ktorý si mládenci prenajali, začali hrať zjednaní muzikanti. Sem donášali matky vykúpaných dievčat „česť&quot; - boli to peniaze, chlieb, koláč — beluš, klobásu a slaninu. Mládenci naliali matke za pohárik pálenky a vykrútili ju v tanci. Večer prichádzali dievky. Ak prišla aj taká, ktorej matka nič nedoniesla, pokrikovali na ňu posmešne, že „česť pýta jesť' Zábava pokračovala aj v utorok.</p></li>
+
+</div>
+
+
+<p>ZDROJ INFORMÁCII: Emília Horváthová: Rok vo zvykoch nášho ľudu, Tatran, 1986</p>
+
+
+<div class=\"fb-share-button\" data-href=\"http://www.ludoslovensky.sk/blog/velka-noc_pat-veci-ktore-ste-nevedeli.php\" data-layout=\"button_count\" data-size=\"large\" data-mobile-iframe=\"true\"><a class=\"fb-xfbml-parse-ignore\" target=\"_blank\" href=\"https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.ludoslovensky.sk%2Fblog%2Fvelka-noc_pat-veci-ktore-ste-nevedeli.php&amp;src=sdkpreparse\">Share</a></div>
+
+<div class=\"fb-comments\" data-href=\"http://www.ludoslovensky.sk/blog/velka-noc_pat-veci-ktore-ste-nevedeli.php\" data-numposts=\"5\"></div>
+
+";
+
+
+$bg_delete=true;
+// nacitanie informacii
+require ($_SERVER["DOCUMENT_ROOT"]."/templates/tmpl_blog_article.php");
+
+?>
