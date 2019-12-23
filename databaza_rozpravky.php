e9abf57a76efe7456f1d249237318b8ae7c8918e
Tomas Ulej1 <ludo@ludoslovensky.sk>
2019-12-23 06:54:52 +0100

    no message

---
+++ databaza_rozpravky.php
@@ -0,0 +1,26 @@
+<?php
+ 
+ if ($_SERVER['APP_ENV']=='tomas') {
+ 
+      $p_meno="root";
+      $p_heslo="LudoLudoVedMaNeser";
+      $p_databaza="rozpravky";
+      $p_host="localhost";
+
+}else if ($_SERVER['APP_ENV'] == "roman") {
+      $p_meno="root";
+      $p_heslo="root";
+      $p_databaza="rozpravky";
+      $p_host="localhost";
+} else {
+      $p_meno="rozpravky";
+      $p_heslo="LudoLudoVedMaNeser";
+      $p_databaza="rozpravky";
+      $p_host="localhost";
+
+}
+      $konstDie = "Chyba pri spojení s databázou. Zlý, zlý Ulej! Ty radšej nič neprogramuj, ale Ulej!";
+                                       //Konstanty
+      mysql_connect($p_host,$p_meno,$p_heslo);
+      mysql_select_db($p_databaza) or die($konstDie);
+?>
