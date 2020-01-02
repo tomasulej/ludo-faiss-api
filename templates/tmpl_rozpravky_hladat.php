6bc4f817501c73af71ea6d6226afd658ab74d164
Tomas Ulej1 <ludo@ludoslovensky.sk>
2020-01-02 11:56:36 +0100

    no message

---
+++ templates/tmpl_rozpravky_hladat.php
@@ -0,0 +1,386 @@
+
+
+<?php
+$theme="l-theme-blue";
+$prislovia_tab='class="active"';
+
+require $_SERVER["DOCUMENT_ROOT"]."/templates/tmpl_header.php";
+require $_SERVER["DOCUMENT_ROOT"]."/templates/tmpl_rozpravky_header.php";
+
+
+?>
+
+<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/instantsearch.js@2.0.0/dist/instantsearch.min.css">
+<script src="https://cdn.jsdelivr.net/npm/instantsearch.js@2.0.0/dist/instantsearch.min.js"></script>
+
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.10&appId=619723681422597";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+
+
+<div class="l-page l-search l-list">
+
+    <div class="container">
+
+        <div class="l-search-header">
+            <h1>Vyhľadávanie v rozprávkach </h1>
+            <!--    <div class="input-group">
+                    <input type="text" class="form-control-lg form-control" id="q" name="q" placeholder="Hľadať v piesňach" <?php if ($_GET['q']<>"") {echo "value='".$_GET['q']."'";} ?>>
+                    <span class="input-group-btn">
+                    <button class="btn btn-lg l-btn--primary" type="button" onclick="$( '#hladat' ).submit();">Hľadať!</button>
+                </span>
+                </div> -->
+
+            
+                <div id="search-box"></div>
+
+
+              <!--      <div class="input-group">
+                <input id="q" type="text" class="form-control-lg form-control" placeholder="Hľadať v rozprávkach">
+                <span class="input-group-btn">
+                    
+                </span>
+            </div>-->
+        
+        
+
+     
+      
+      <div id="current-refined-values">
+  <!-- CurrentRefinedValues widget will appear here -->
+</div>
+
+
+        </div>
+
+    </div>
+
+
+
+    <div class="container">
+<div class="row">
+          <div class="col-md-4 l-list-filter">
+
+
+
+
+
+               <div class="list-group">
+                    <span href="#" class="list-group-item list-group-item-action ">
+                        <h3 class="list-group-item-heading">Dĺžka čítania:</h3>
+                        
+                        <div id="filter_time">
+                            <!-- RefinementList widget will appear here -->
+                        </div>
+                        
+                    </span>    
+                        
+
+
+                </div>
+
+
+
+
+                <div class="list-group">
+                    <span href="#" class="list-group-item list-group-item-action ">
+                        <h3 class="list-group-item-heading">Autor (zberateľ)</h3>
+                        
+                        <div id="filter_autor">
+                            <!-- RefinementList widget will appear here -->
+                        </div>
+                        
+                    </span>    
+                        
+
+
+                </div>
+
+                 <div class="list-group">
+                    <span href="#" class="list-group-item list-group-item-action ">
+                        
+                        <h3 class="list-group-item-heading">Názov diela:</h3>
+
+
+                        <div id="filter_title">
+                            <!-- RefinementList widget will appear here -->
+                        </div>
+                        
+                    </span>    
+                        
+
+
+                </div>
+
+                    
+                
+
+
+
+            </div>
+
+
+
+<div class="col-md-8">
+
+
+
+
+
+
+        <div class="l-list-items">
+            <ul class="list-group">
+            <div id="hits">
+                
+            
+            </div>
+
+
+
+            </ul>
+
+            <div id="strankovanie"></div>
+
+        </div>
+</div>
+
+
+
+</div>
+        </div>
+
+
+
+    </div>
+
+
+<script>
+
+
+
+  
+
+
+
+</script>
+
+<script>
+  
+const search = instantsearch({
+  appId: '35TGB7A4IL',
+  apiKey: 'd6d60f1618cfe0cb63e229b4678fc703',
+  indexName: 'rozpravky',
+  urlSync: true
+});
+
+search.start();
+
+
+
+
+
+
+
+  // initialize RefinementList
+  search.addWidget(
+    instantsearch.widgets.refinementList({
+      container: '#filter_time',
+      attributeName: 'r_time_kat',
+      limit: 3,
+    showMore: {
+        limit:10,
+        templates: {
+            inactive: '<div><small><a href="javascript:undefined"><i class="fa fa-angle-double-down"></i> Zobraziť viac</a></small></div>',
+            active: ""
+        }
+    }, 
+  
+        templates: {
+            item:    '<input class="{{cssClasses.checkbox}}" type="checkbox" value="{{value}}" {{#isRefined}}checked{{/isRefined}}>  {{value}} <sup>{{count}}</sup>',
+ 
+
+        },
+        cssClasses: {
+            header: 'd-inline-block',
+            //body: 'd-inline-block',
+            //item: 'd-inline-block '
+
+        }
+
+
+    })
+  );
+
+  // initialize RefinementList
+  search.addWidget(
+    instantsearch.widgets.refinementList({
+      container: '#filter_autor',
+      attributeName: 'pp_author',
+      limit: 3,
+    showMore: {
+        limit:10,
+        templates: {
+            inactive: '<div><small><a href="javascript:undefined"><i class="fa fa-angle-double-down"></i> Zobraziť viac</a></small></div>',
+            active: ""
+        }
+    }, 
+  
+        templates: {
+            item:    '<input class="{{cssClasses.checkbox}}" type="checkbox" value="{{value}}" {{#isRefined}}checked{{/isRefined}}>  {{value}} <sup>{{count}}</sup>',
+ 
+
+        },
+        cssClasses: {
+            header: 'd-inline-block',
+            //body: 'd-inline-block',
+            //item: 'd-inline-block '
+
+        }
+
+
+    })
+  );
+
+
+  // initialize RefinementList
+  search.addWidget(
+    instantsearch.widgets.refinementList({
+      container: '#filter_title',
+      attributeName: 'pp_title',
+      limit: 3,
+    showMore: {
+        limit:10,
+        templates: {
+            inactive: '<div><small><a href="javascript:undefined"><i class="fa fa-angle-double-down"></i> Zobraziť viac</a></small></div>',
+            active: ""
+        }
+    }, 
+  
+        templates: {
+            item:    '<input class="{{cssClasses.checkbox}}" type="checkbox" value="{{value}}" {{#isRefined}}checked{{/isRefined}}>  {{value}} <sup>{{count}}</sup>',
+ 
+
+        },
+        cssClasses: {
+            header: 'd-inline-block',
+            //body: 'd-inline-block',
+            //item: 'd-inline-block '
+
+        }
+
+
+    })
+  );
+
+
+
+
+
+
+  search.addWidget(
+    instantsearch.widgets.hits({
+      container: '#hits',
+      hitsPerPage: 8,
+      templates: {
+        empty: 'No results',
+        item: '<div class="l-song-item l-well"><h3><a href="rozpravka.php?id={{id}}">{{{_highlightResult.r_nazov.value}}}</a> <small>({{r_time}} min čítania)</small></h3><div class="row"><div class="col-md-12 hidden-sm-down"><p>{{{_snippetResult.r_text.value}}}</p><a  href="rozpravka.php?id={{id}}" class="l-btn l-btn--primary l-btn--small">Zobraziť celú rozprávku</a></div></div></div>'
+      }
+    })
+  );
+
+
+
+  // initialize SearchBox
+  search.addWidget(
+    instantsearch.widgets.searchBox({
+      container: '#search-box',
+      wrapInput: false,
+      placeholder: 'Hľadaj',
+      cssClasses: {input: 'form-control-lg form-control', root: 'input-group'},
+      poweredBy: true,
+
+    })
+  );
+
+
+
+
+
+
+
+
+  search.addWidget(
+    instantsearch.widgets.currentRefinedValues({
+      container: '#current-refined-values',
+      cssClasses: {
+          body: 'd-inline-block',
+          item: 'tag d-inline-block',
+          link: 'tag tag-default fa fa-close'
+      },
+      templates: {
+          header: '<Strong> Filtre: </strong>'
+          //clearAll: '<div><small><a href="{{url}}"><i class="fa fa-angle-double-down"></i> Vymazať všetky filtre</a></small></div>'
+
+      },
+      // This widget can also contain a clear all link to remove all filters,
+      // we disable it in this example since we use `clearAll` widget on its own.
+      clearAll: false
+    })
+  );
+
+
+
+search.addWidget(
+  instantsearch.widgets.pagination({
+    container: '#strankovanie',
+    cssClasses: {
+        root: 'pagination',
+        item: 'page-item',
+        link: 'page-link',
+        active: 'active'
+    },
+    scrollTo: 'h2',
+    labels: {
+       next: 'Ďalšia strana >',
+       previous: '< Predchádzajúca' 
+    },
+    maxPages: 20,
+    // default is to scroll to 'body', here we disable this behavior
+    scrollTo: false,
+    showFirstLast: false,
+  })
+);
+
+
+
+  search.start();
+
+
+    function playpause(media_id,button_id) 
+    {
+     if ($(media_id)[0].paused) {
+        $(media_id).trigger('play');
+        $(button_id+' i').attr('class', "fa fa-pause");
+    
+     } else {    
+        $(media_id).trigger('pause');
+        $(button_id+' i').attr('class', "fa fa-play");
+     }    
+    }
+
+
+</script>
+
+<?php require $_SERVER["DOCUMENT_ROOT"]."/templates/tmpl_footer.php";?>
+
+
+
+
+
