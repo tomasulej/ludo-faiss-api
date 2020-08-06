482422d545511579dfb22c2cb23002232214322c
Tomáš Ulej <tomas.ulej@martinus.sk>
2020-08-06 09:31:16 +0200

    no message

--- kategorie/kniha-roka.php
+++ kategorie/kniha-roka.php
@@ -1353,7 +1353,8 @@
                 <div class="row mb-large">
                   <div class="col--m-12 flex mb-large">
                     
-                  <style>.embed-container { position: relative; padding-bottom:56.25%; height:0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%;}</style><div class='embed-container' data-page-width='451' data-page-height='640' id='ypembedcontainer' ><iframe   src="https://www.yumpu.com/xx/embed/view/y4GNLKC5Qbd0rDhw" frameborder="0" allowfullscreen="true"  allowtransparency="true"></iframe></div><script src='https://players.yumpu.com/modules/embed/yp_r_iframe.js' ></script>
+                  <style>.embed-container { position: relative; padding-bottom:56.25%; height:0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%;}</style><div class='embed-container' data-page-width='451' data-page-height='640' id='ypembedcontainer' ><iframe   src="https://www.yumpu.com/cs/embed/view/MI6NOXoFgxfbK9Wl" frameborder="0" allowfullscreen="true"  allowtransparency="true"></iframe></div><script src='https://players.yumpu.com/modules/embed/yp_r_iframe.js' ></script>
+
                 </div>
 
               </div>
